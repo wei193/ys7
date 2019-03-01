1064a3fbcb8dc43f6cbe5afb0c889e0e442a5ad4
chenwochong <1057364182@qq.com>
2019-03-01 12:58:12 +0800

    优化type.go缩进

--- type.go
+++ type.go
@@ -37,27 +37,27 @@ type Device struct {
 //DeviceCapacity 萤石设备能力集
 type DeviceCapacity struct {
 	Support_cloud					string `json:"support_cloud"`
-	Support_intelligent_track		string `json:"support_intelligent_track"`
+	Support_intelligent_track			string `json:"support_intelligent_track"`
 	Support_p2p_mode				string `json:"support_p_2_p_mode"`
 	Support_resolution				string `json:"support_resolution"`
 	Support_talk					string `json:"support_talk"`
 	Support_wifi_userId				string `json:"support_wifi_user_id"`
-	Support_remote_auth_randcode	string `json:"support_remote_auth_randcode"`
+	Support_remote_auth_randcode			string `json:"support_remote_auth_randcode"`
 	Support_upgrade					string `json:"support_upgrade"`
 	Support_smart_wifi				string `json:"support_smart_wifi"`
-	Support_ssl						string `json:"support_ssl"`
+	Support_ssl 					string `json:"support_ssl"`
 	Support_weixin					string `json:"support_weixin"`
 	Ptz_close_scene					string `json:"ptz_close_scene"`
-	Support_preset_alarm			string `json:"support_preset_alarm"`
-	Support_related_device			string `json:"support_related_device"`
+	Support_preset_alarm				string `json:"support_preset_alarm"`
+	Support_related_device				string `json:"support_related_device"`
 	Support_message					string `json:"support_message"`
-	Ptz_preset						string `json:"ptz_preset"`
+	Ptz_preset  					string `json:"ptz_preset"`
 	Support_wifi					string `json:"support_wifi"`
-	Support_cloud_version			string `json:"support_cloud_version"`
+	Support_cloud_version				string `json:"support_cloud_version"`
 	Ptz_center_mirror				string `json:"ptz_center_mirror"`
 	Support_defence					string `json:"support_defence"`
 	Ptz_top_bottom					string `json:"ptz_top_bottom"`
-	Support_fullscreen_ptz			string `json:"support_fullscreen_ptz"`
+	Support_fullscreen_ptz				string `json:"support_fullscreen_ptz"`
 	Support_defenceplan				string `json:"support_defenceplan"`
 	Support_disk					string `json:"support_disk"`
 	Support_alarm_voice				string `json:"support_alarm_voice"`
@@ -66,7 +66,7 @@ type DeviceCapacity struct {
 	Support_capture					string `json:"support_capture"`
 	Support_privacy					string `json:"support_privacy"`
 	Support_encrypt					string `json:"support_encrypt"`
-	Support_auto_offline			string `json:"support_auto_offline"`
+	Support_auto_offline				string `json:"support_auto_offline"`
 }
 
 //DeviceInfo 萤石设备数据结构
@@ -96,7 +96,7 @@ type Camera struct {
 
 //Account 萤石子账号ID
 type Account struct {
-	AccountID string `json:"accountId"`
+	AccountID 	string `json:"accountId"`
 }
 
 //RAMAccount 萤石子账号
@@ -173,37 +173,37 @@ type Picture struct {
 // 设备状态信息
 type DeviceStatusInfo struct {
 	PrivacyStatus	int `json:"privacyStatus"`
-	PirStatus		int `json:"pirStatus"`
+	PirStatus   	int `json:"pirStatus"`
 	AlarmSoundMode  int `json:"alarmSoundMode"`
 	BattryStatus	int `json:"battryStatus"`
-	LockSignal		int `json:"lockSignal"`
-	DiskNum			int `json:"diskNum"`
-	DiskState		string `json:"diskState"`
-	CloudType		int `json:"cloudType"`
-	CloudStatus		int `json:"cloudStatus"`
-	NvrDiskNum		int `json:"nvrDiskNum"`
-	NvrDiskState	string `json:"nvrDiskState"`
+	LockSignal  	int `json:"lockSignal"`
+	DiskNum      	int `json:"diskNum"`
+	DiskState   	string `json:"diskState"`
+	CloudType   	int `json:"cloudType"`
+	CloudStatus  	int `json:"cloudStatus"`
+	NvrDiskNum  	int `json:"nvrDiskNum"`
+	NvrDiskState  	string `json:"nvrDiskState"`
 }
 
 // SwitchStatus 开关状态
 type SwitchStatus struct {
-	DeviceSerial string `json:"deviceSerial"`
-	ChannelNo    int    `json:"channelNo"`
-	Enable		 int	`json:"enable"`
+	DeviceSerial	string `json:"deviceSerial"`
+	ChannelNo   	int    `json:"channelNo"`
+	Enable       	int    `json:"enable"`
 }
 
 //TrafficTotal 账号下流量消耗汇总
 type TrafficTotal struct {
-	TotalFlow		int64 `json:"totalFlow"`
-	UsedFlow		int64 `json:"usedFlow"`
-	AverageConsume	int   `json:"averageConsume"`
+	TotalFlow       int64 `json:"totalFlow"`
+	UsedFlow        int64 `json:"usedFlow"`
+	AverageConsume  int   `json:"averageConsume"`
 }
 
 //TrafficUserDetail 账户下流量消耗详情
 type TrafficUserDetail struct {
 	FlowDate		int64  `json:"flowDate"`
-	DeviceCount		int	   `json:"deviceCount"`
-	ChannelCount	int    `json:"channelCount"`
+	DeviceCount		int    `json:"deviceCount"`
+	ChannelCount		int    `json:"channelCount"`
 	HlsFlow			int64  `json:"hlsFlow"`
 	AppFlow			int64  `json:"appFlow"`
 	RtmpFlow		int64  `json:"rtmpFlow"`
